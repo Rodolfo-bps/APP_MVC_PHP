ab5ab950af693fdb8cccdb201b91fef26d877ae6
Rodolfo-bps <rodolfo.velazquezbps@gmail.com>
2024-01-13 10:55:35 -0600

    plantilla y vistas

---
+++ config/APP.php
@@ -0,0 +1,10 @@
+<?php 
+const SERVERURL = "http://localhost/APP_MVC_PHP-main/";
+
+const COMPANY = "SISTEMAS PRESTAMOS";
+
+const MONEDA = "$";
+
+date_default_timezone_set("America/Mexico_City");
+
+?>
